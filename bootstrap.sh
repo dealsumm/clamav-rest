4e7e0d16d3a5529f3e17a6899d05ee5a785ea482
lior-neuman <liorn@dealsumm.com>
2019-12-08 15:18:24 +0200

    Added timeout configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,15 @@ set -m
 
 host=${CLAMD_HOST:-192.168.50.72}
 port=${CLAMD_PORT:-3310}
+timeout=${CLAMD_TIMEOUT:-500}
 maxfilesize=${CLAMD_MAX_FILE_SIZE:-20000KB}
 maxrequestsize=${CLAMD_MAX_REQUEST_SIZE:-20000KB}
 
 echo "using clamd server: $host:$port"
 echo "Max file size: $maxfilesize"
 echo "Max request size: $maxrequestsize"
+echo "Timeout: $timeout"
 
 # start in background
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$maxfilesize --clamd.maxrequestsize=$maxrequestsize
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$maxfilesize --clamd.maxrequestsize=$maxrequestsize --clamd.timeout=$timeout
 
